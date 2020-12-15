7a141a8118c5dfd63910654385e30ed7e1de1b3b
Yann <yann.trou@gmail.com>
2020-12-15 14:09:39 +0100

    implémentation du cache

--- manage.sh
+++ manage.sh
@@ -1,96 +1,153 @@
-#!/bin/bash
+#!/bin/zsh
 
-mod = "null"
+if [ ! -f cache ]
+then
+    echo "cache inexistant, reconstruction"
+    touch cache
+    echo "default.bib" >> cache
+    echo "BASE" >> cache
+fi
 
-while getopt hbra:c:ld:un OPT
+base=`gsed -n '1p' cache`
+mod=`gsed -n '2p' cache`
+echo "base: $base"
+echo "mod: $mod"
+
+function usage()
+{
+    echo "-b => action sur les bases"
+    echo "-r => action sur les références"
+    echo "./manage.sh [-b| -r| -a| -c| -l| -d| -u| -n]"
+    echo "-a <base|reference> => ajouter base/ref"
+    echo "-d <nom de la référence> => supprimer une référence contenue dans la base"
+}
+
+function createBase()
+{
+    readonly baseName=$1
+    touch "bases/$baseName.bib"
+    echo "base $baseName.bib crée dans bases/"
+}    
+
+function addReference()
+{
+    reference=$1
+    if [ -f $reference ]
+    then 
+        cat $reference >> bases/$base
+        echo "a écrit le contenu de $reference dans $base"
+
+        refID=`grep -E "@" $reference`
+        size=`wc -l $reference | tr -d "[:alpha:]" | tr -d "[:space:]"`
+        echo "ref::$refID::$size::$base" >> cache
+        echo "référence enregistrée dans le cache"
+    else
+        echo $reference >> $base
+        echo "a ajouté la référence dans $base"
+    fi
+    
+}
+
+function removeReference()
+{
+    refID=$1
+    refSize=`awk 'BEGIN { fs="::" } $2 ~ ID{ print $3 }' cache ID=$refID`
+    echo $refSize
+}
+
+function detectDoublon()
+{
+    echo "detectDoublon"
+}
+
+function changeBase()
+{
+    newBase=$1
+    gsed -i "1 c\\$newBase" cache
+}
+
+function listBases()
+{
+    ls bases
+}
+
+function listReferences()
+{
+    echo "begin"
+    grep -E "@" $base
+    echo "end"
+}
+
+function findReference()
+{
+    if [ -f $base ]
+    then 
+        refToFind=$1
+        echo "start"
+        grep -E "$refToFind" $base
+    fi
+}
+
+function clean()
+{
+    echo "clean()"
+}
+
+
+while getopts hbra:c:ld:unf: OPT
 do
     case $OPT in
         h)
-            usage()
+            usage
             exit 0
             ;;
-        b)
-            $mod = "BASE"
+        b) 
+            gsed -i "2c\BASE" cache
+            echo "gestion de la base $base"
             ;;
         r)
-            $mod = "REF"
+            gsed -i "2c\REF" cache
+            echo "gestion des références de la base $base"
             ;;
         a)
-            if [ $mod == "BASE"]
+            if [ $mod = 'BASE' ]
             then
-                readonly baseName = $OPTARG
-                createBase()
-            elif [ $mod == "REF" ]
+                createBase $OPTARG
+            elif [ $mod = 'REF' ]
             then 
-                readonly ref = $OPTARG
-                addReference()
+                addReference $OPTARG
             else
-                echo "Erreur, option < -b | -r > manquante"
-                usage();
+                echo "Erreur, mode de gestion non sélectionné"
+                usage
                 exit 1
             fi
             ;;
         d)
-            readonly refToDelete = $OPTARG
-            removeReference()
+            removeReference $OPTARG
+            ;;
+        f)
+            findReference $OPTARG
             ;;
         u)
-            detectDoublon()
+            detectDoublon
             ;;
         c)
-            readonly baseToOpen = $OPTARG
-            changeBase()
+            changeBase $OPTARG
             ;;
         l)
-            listBases()
+            if [ $mod = "BASE" ]
+            then 
+                listBases
+            elif [ $mod = 'REF' ]
+            then
+                listReferences
+            else 
+                echo "Erreur, mode de gestion non sélectionné"
+                exit 1
+            fi
             ;;
         n)
-            clean()
-            ;;
-        *)
-            echo "Erreur, paramètre(s) invalide(s), arrêt"
-            usage()
-            exit 1
+            clean
             ;;
     esac
 done
-
-usage()
-{
-
-}
-
-createBase()
-{
-
-}    
-
-addReference()
-{
-
-}
-
-removeReference()
-{
-
-}
-
-detectDoublon()
-{
-    
-}
-
-changeBase()
-{
-
-}
-
-listBases()
-{
-
-}
-
-clean()
-{
-
-}
